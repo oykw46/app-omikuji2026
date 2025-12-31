fbd20f4e6ab1441cc81ac33acff34b49c1140a8b
oyakawa <oyakawa.masaki.abnet@gmail.com>
2025-12-31 16:21:36 +0900

    tuning shop

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -80,7 +80,7 @@ export default function ShopPage() {
                     </div>
                 </div>
 
-                <div className='overflow-y-scroll shop-item-list pb-20'>
+                <div className='overflow-y-scroll shop-item-list pb-24'>
                     {/* 商品リスト */}
                     <div className='flex flex-col pt-4'>
                         {shopItems.map((item) => {
