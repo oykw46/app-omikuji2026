fd8bfe58de846baa87acea2f12e217b5a93eb2ba
oyakawa <oyakawa.masaki.abnet@gmail.com>
2025-12-31 14:58:09 +0900

    tuning result

--- app/components/FortuneResult.tsx
+++ app/components/FortuneResult.tsx
@@ -74,7 +74,7 @@ export const FortuneResult = ({
 				<div className='relative shadow-lg '>
 					<div className='absolute inset-0 bg-white opacity-80'></div>
 					<h1 className='text-lg text-center font-bold py-6 text-red-600 old-standard-tt-bold relative'>{fortune.level}</h1>
-					<div className='space-y-6 text-left py-4 overflow-y-scroll max-w-full relative jp-vertical scrollbar-none max-sm:max-h-60 max-sm:h-100'>
+					<div className='space-y-6 text-left px-0 py-8 overflow-y-scroll max-w-full relative jp-vertical scrollbar-none max-sm:max-h-60 max-sm:h-100'>
 						{fortuneItems.map((item, index) => (
 							<FortuneLetter 
 								key={index}
